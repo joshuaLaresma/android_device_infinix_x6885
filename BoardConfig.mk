bf7badb2732a9404c7d7054312ff68c287c7d37f
joshuaLaresma <158560563+joshuaLaresma@users.noreply.github.com>
2026-08-02 00:45:37 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-DEVICE_PATH := device/generic/vendor_boot
+DEVICE_PATH := device/infinix/X6885
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
