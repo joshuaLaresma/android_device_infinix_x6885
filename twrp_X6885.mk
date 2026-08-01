28acd8447bf0e4047c29f5d0609346d37cca1431
joshuaLaresma <158560563+joshuaLaresma@users.noreply.github.com>
2026-08-02 07:06:28 +0800

    Update twrp_X6885.mk

--- twrp_X6885.mk
+++ twrp_X6885.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common OrangeFox stuff.
-$(call inherit-product, vendor/orangefox/config/common.mk)
+$(call inherit-product, vendor/recovery/config/common.mk)
 
 # Inherit from vendor_boot device
 $(call inherit-product, device/infinix/X6885/device.mk)
