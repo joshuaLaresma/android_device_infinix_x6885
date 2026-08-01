460b90c8a8d07e514a38bec764d5ecb2879befff
joshuaLaresma <158560563+joshuaLaresma@users.noreply.github.com>
2026-08-01 23:08:42 +0800

    Add files via upload

---
+++ omni_vendor_boot.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from vendor_boot device
+$(call inherit-product, device/generic/vendor_boot/device.mk)
+
+PRODUCT_DEVICE := vendor_boot
+PRODUCT_NAME := omni_vendor_boot
+PRODUCT_BRAND := generic
+PRODUCT_MODEL := Generic Device
+PRODUCT_MANUFACTURER := generic
+
+PRODUCT_GMS_CLIENTID_BASE := android-generic
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="hal_mgvi_64_64only_armv82_for_ota-user 16 BP2A.250605.031.A3 116549 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X6885-OPPJ/Infinix-X6885:16/BP2A.250605.031.A3/301300002FANS:user/release-keys
+
